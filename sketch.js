02d1d0f0a286aeb61c2f5190f58dbcaea9610f7f
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-02-22 10:44:16 -0300

    22/02/2022

--- sketch.js
+++ sketch.js
@@ -1,30 +1,30 @@
 var rex, rexCorrendo;
-
+var terra ;
+var terra_plana
 function preload(){
-  
-  rexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
-
+rexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
+terra_plana = loadImage ("ground2.png");
 }
-
 function setup(){
-  createCanvas(600,200)
-  
-  rex = createSprite(50, 160, 20, 50);
-  rex.scale = 0.5;
-  rex.addAnimation("correndo", rexCorrendo);
- 
-  borda = createEdgeSprites();
+createCanvas(600,200)
+rex = createSprite(50, 160, 20, 50);
+rex.scale = 0.5;
+rex.addAnimation("correndo", rexCorrendo);
+borda = createEdgeSprites();
+terra = createSprite(200, 180, 400, 20) ;
+terra.addImage (terra_plana) ;
+terra.x = terra.width/2 ; 
 }
-
 function draw(){
-  background("lightgrey");
-
-  if(keyDown("space")){
-    rex.velocityY = -10;
-  }
-  rex.velocityY += 1;
-
-  rex.collide(borda[3]);
-  
-  drawSprites();
+background("white");
+if(keyDown("space")){
+rex.velocityY = -10;
+}
+rex.velocityY += 1;
+terra.velocityX = -2; 
+if(terra.x < 0){
+terra.x = terra.width/2 ;
+}
+rex.collide(terra);
+drawSprites();
 }
