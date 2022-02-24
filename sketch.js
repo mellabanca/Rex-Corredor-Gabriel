61ad29dd323927cfeabddfb5cb3cbd99b0427675
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-02-24 14:22:52 -0300

    24/02/2022

--- sketch.js
+++ sketch.js
@@ -1,30 +1,62 @@
 var rex, rexCorrendo;
 var terra ;
 var terra_plana
+var invisiblie_ground
+
+
 function preload(){
 rexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
 terra_plana = loadImage ("ground2.png");
+
 }
+
 function setup(){
+
 createCanvas(600,200)
+
 rex = createSprite(50, 160, 20, 50);
+
 rex.scale = 0.5;
+
 rex.addAnimation("correndo", rexCorrendo);
+
 borda = createEdgeSprites();
+
 terra = createSprite(200, 180, 400, 20) ;
+
+invisiblie_ground = createSprite (200, 190, 400, 10) ;
+
+invisiblie_ground.visible = false ;
+
 terra.addImage (terra_plana) ;
+
 terra.x = terra.width/2 ; 
+
+//var aleatorio = Math.round(random(1,100));
+//console.log(aleatorio);
+
 }
+
 function draw(){
 background("white");
-if(keyDown("space")){
+//console.log (rex.y) ;
+console.log(frameCount);
+
+if(keyDown("space")&& rex.y >= 145){
 rex.velocityY = -10;
 }
+
 rex.velocityY += 1;
 terra.velocityX = -2; 
 if(terra.x < 0){
 terra.x = terra.width/2 ;
 }
-rex.collide(terra);
+
+rex.collide(invisiblie_ground);
+cloud();
 drawSprites();
+}
+
+function cloud(){
+
 }
