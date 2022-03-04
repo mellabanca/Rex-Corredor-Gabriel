6b102f398e8a974cb827154d972e2485ceeafee3
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-03-04 16:16:58 -0300

    04/03/2022

--- sketch.js
+++ sketch.js
@@ -4,15 +4,30 @@ var terra_plana ;
 var invisiblie_ground ;
 var clouds ;
 var joinville_chove_muito
-
-
+var cacto1
+var cacto2
+var cacto3
+var cacto4
+var cacto5
+var cacto6
+var points
+var grupoNuvens;
+var grupoCactos;
+var JOGANDO = 1;
+var PERDEU = 0;
+var estado = JOGANDO;
 
 
 function preload(){
 rexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
 terra_plana = loadImage ("ground2.png");
 joinville_chove_muito = loadImage ("cloud.png") ;
-
+cacto1 = loadImage("obstacle1.png") 
+cacto2 = loadImage("obstacle2.png")
+cacto3 = loadImage("obstacle3.png")
+cacto4 = loadImage("obstacle4.png")
+cacto5 = loadImage("obstacle5.png")
+cacto6 = loadImage("obstacle6.png")
 
 
 }
@@ -39,18 +54,34 @@ terra.addImage (terra_plana) ;
 
 terra.x = terra.width/2 ; 
 
+points = 0
+
+grupoNuvens = new Group();
+
+grupoCactos = new Group();
+
+
 //var aleatorio = Math.round(random(1,100));
 //console.log(aleatorio);
 
 }
 
 function draw(){
-background("white");
-//console.log (rex.y) ;
-//console.log(frameCount);
+   background("white");
+   //console.log (rex.y) ;
+   //console.log(frameCount);
+
+if(estado === JOGANDO){
+
+} else if (estado === PERDEU){
+
+}
+
+
+
 
 if(keyDown("space")&& rex.y >= 145){
-rex.velocityY = -10;
+rex.velocityY = -11;
 }
 
 rex.velocityY += 1;
@@ -61,7 +92,10 @@ terra.x = terra.width/2 ;
 
 rex.collide(invisiblie_ground);
 cloud();
+cactos();
 drawSprites();
+points += Math.round(frameCount / 60) ;
+text(points, 500, 50) ;
 }
 
 function cloud(){
@@ -78,10 +112,40 @@ rex.depth += 1 ;
 
 clouds.lifetime = 250;
 
+grupoNuvens.add(clouds);
+
+}}
+function cactos (){
+if(frameCount % 60 ===0){
+var planta = createSprite(600, 165, 10, 40) ;
+planta.velocityX = -6
+var infinito = Math.round(random(1, 6)) ;
+switch (infinito) {
+     case 1:planta.addImage(cacto1);
+        break;
+     case 2:planta.addImage(cacto2);
+        break;
+     case 3:planta.addImage(cacto3);
+        break;
+     case 4:planta.addImage(cacto4);
+        break;
+     case 5:planta.addImage(cacto5);
+        break;
+     case 6:planta.addImage(cacto6) ;
+        break;
+
+    default:
+        break;
+}
+planta.scale = 0.5 ;
+planta.lifetime = 300 ;
+
+grupoCactos.add(planta);
+
+}
+
+
 }
 
 
 
-
-
-}
